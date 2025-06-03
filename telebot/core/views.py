f4ecbff3082b386f77c7122ec8b23be12d788cdd
Aakash yadav <126560106+aakashy5990@users.noreply.github.com>
2025-06-03 16:20:27 +0530

    e

--- telebot/core/views.py
+++ telebot/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+import os
+from django.http import HttpResponse
+
+def show_project_files(request):
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # adjust as needed
+    files = os.listdir(base_dir)
+    return HttpResponse('<br>'.join(files))
